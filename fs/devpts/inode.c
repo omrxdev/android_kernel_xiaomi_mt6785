a2581b7351e2808b92cef50bcf97a4940441e780
omrxdev <omarmohammed3147@gmail.com>
2026-05-27 15:28:16 +0300

    comment out devpts code

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -602,12 +602,12 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
-#ifdef CONFIG_KSU_SUSFS
-extern int ksu_handle_devpts(struct inode*);
-#endif
-#ifdef CONFIG_KSU
-extern int ksu_handle_devpts(struct inode*);
-#endif
+// #ifdef CONFIG_KSU_SUSFS
+// extern int ksu_handle_devpts(struct inode*);
+// #endif
+// #ifdef CONFIG_KSU
+// extern int ksu_handle_devpts(struct inode*);
+// #endif
 
 /**
  * devpts_get_priv -- get private data for a slave
@@ -617,16 +617,16 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
-#ifdef CONFIG_KSU_SUSFS
-	if (likely(susfs_is_current_proc_umounted())) {
-		goto orig_flow;
-	}
-	ksu_handle_devpts(dentry->d_inode);
-orig_flow:
-#endif
-        #ifdef CONFIG_KSU
-        ksu_handle_devpts(dentry->d_inode);
-        #endif
+// #ifdef CONFIG_KSU_SUSFS
+// 	if (likely(susfs_is_current_proc_umounted())) {
+// 		goto orig_flow;
+// 	}
+// 	ksu_handle_devpts(dentry->d_inode);
+// orig_flow:
+// #endif
+        // #ifdef CONFIG_KSU
+        // ksu_handle_devpts(dentry->d_inode);
+        // #endif
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
